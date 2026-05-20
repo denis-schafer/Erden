dbe712406a3e91383dfa58c4a5a05170cf37c9e0
Denis Schafer <denischafer@gmail.com>
2026-05-20 20:23:02 -0300

    Fix: restore missing Dispatchable and SerializesModels imports

--- app/Events/PosQRUpdated.php
+++ app/Events/PosQRUpdated.php
@@ -5,6 +5,8 @@ namespace App\Events;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class PosQRUpdated implements ShouldBroadcastNow
 {

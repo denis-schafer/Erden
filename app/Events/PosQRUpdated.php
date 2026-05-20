b138c9f685cbd7ee2094ac0ff81248796c9a6f33
Denis Schafer <denischafer@gmail.com>
2026-05-20 20:10:42 -0300

    Change PosQRUpdated to ShouldBroadcastNow (immediate, bypass queue)

--- app/Events/PosQRUpdated.php
+++ app/Events/PosQRUpdated.php
@@ -4,11 +4,9 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class PosQRUpdated implements ShouldBroadcast
+class PosQRUpdated implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
